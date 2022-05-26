dcb32b2bcc66453f8629a011366b391bedbf9dbe
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2022-05-26 10:16:05 +0200

    re-organize files for sift and orb

--- sift_orb__tracker_lk.py
+++ sift_orb__tracker_lk.py
@@ -2,10 +2,15 @@ import cv2
 import numpy as np
 
 webcam = False
+sift = False
 NUM_FEATURES = 80
 
 n_frames = 0
-sift = cv2.SIFT_create(NUM_FEATURES, 6, 0.1)
+detector = None
+if sift:
+    detector = cv2.SIFT_create(NUM_FEATURES, 6, 0.1)
+else:
+    detector = cv2.ORB_create(NUM_FEATURES)
 
 if webcam:
     video = cv2.VideoCapture(0)
@@ -38,22 +43,24 @@ def getResizedFrame(video, ratio):
 
 # detects keypoints and draws them on frame
 def detectKeypoints(frame):
-    global sift
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    global detector
+    work_frame = frame.copy()
+    gray_frame = cv2.cvtColor(work_frame, cv2.COLOR_BGR2GRAY)
     #detect keypoints and draw them on screen
-    kps, dsc = sift.detectAndCompute(frame, None)
-    frame = cv2.drawKeypoints(gray_frame, kps, frame, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    kps, dsc = detector.detectAndCompute(work_frame, None)
+    work_frame = cv2.drawKeypoints(gray_frame, kps, work_frame, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     corners_predicted = cv2.KeyPoint.convert(kps)
-    return frame, corners_predicted
+    return work_frame, corners_predicted
 
 # draws circles over the predicted corners
 def drawCircleCorners(frame, corners_predicted, radius, colour, thickness):
+    work_frame = frame.copy()
     for item in corners_predicted.astype(int):
         x, y = item
         x = int(x)
         y = int(y)
-        cv2.circle(frame, (x, y), radius, colour, thickness)
-    return frame
+        cv2.circle(work_frame, (x, y), radius, colour, thickness)
+    return work_frame
 
 # returns details regarding how many corners are lost
 def getLoss(corners_predicted):
@@ -79,28 +86,31 @@ MAIN CODE
 """
 
 # I read the first frame and get the first set of keypoints
-frame = getResizedFrame(video, 5)
-frame, corners_predicted = detectKeypoints(frame)
+clear_frame = getResizedFrame(video, 5)
+work_frame, corners_predicted = detectKeypoints(clear_frame)
 print(f"initially detected: {len(corners_predicted)}")
+# Displaying the image
+cv2.imshow("Video", work_frame)
 
 while video.isOpened():
     # I keep information about the previous processing (will be useful for the prediction)
-    prev_frame = frame.copy()
+    prev_frame = clear_frame.copy()
     prev_corners = corners_predicted
 
     # read the next frame and predict the new position of the corners
-    frame = getResizedFrame(video, 5)
-    corners_predicted, status_predicted, err = cv2.calcOpticalFlowPyrLK(prev_frame, frame, prev_corners, None)
-    frame = drawCircleCorners(frame, corners_predicted, 6, (0, 255, 0), 1)
-    print(f"n_frames: {n_frames}")
+    clear_frame = getResizedFrame(video, 5)
+    corners_predicted, status_predicted, err = cv2.calcOpticalFlowPyrLK(prev_frame, clear_frame, prev_corners, None)
+    work_frame = drawCircleCorners(clear_frame, corners_predicted, 6, (0, 255, 0), 1)
 
     # check the lost corners and, in case, detect new keypoints
     loss_points = getLoss(corners_predicted)
     if(loss_points["total"] > len(corners_predicted)/2):
-        _, corners_predicted = detectKeypoints(frame)
+        _, corners_predicted = detectKeypoints(clear_frame)
+
+    print(f"nth_frame: {n_frames} | loss: {loss_points['total']}")
 
     # Displaying the image
-    cv2.imshow("Video", frame)
+    cv2.imshow("Video", work_frame)
     if cv2.waitKey(1) == ord('q'):
         break
 
